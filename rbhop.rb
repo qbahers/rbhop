540980ed5cd511de4b33336d2ae7ff53642a51be
Quentin Bahers <quentin.bahers@gmail.com>
2014-04-01 15:53:39 +0200

    Declare the 'operators' and 'methods' variables as globals

--- rbhop.rb
+++ rbhop.rb
@@ -11,6 +11,10 @@ end
 
 class Goal
   attr_accessor :name
+
+  def deep_clone
+    Marshal::load(Marshal.dump(self))
+  end
 end
 
 ### print_state and print_goal are identical except for the name
@@ -40,35 +44,34 @@ end
 ############################################################
 ## Commands to tell Pyhop what the operators and methods are
 
-operators = {}
-methods = {}
+$operators = {}
+$methods = {}
 
-def declare_operators(*op_list, operators)
-  op_list.map do |op|
-    operators[op] = method(op.to_sym)
-  end
+def declare_operators(*op_list)
+  op_list.map { |op| $operators[op] = method(op.to_sym) }
+  $operators
 end
 
-def declare_methods(task_name, *method_list, methods)
-  methods[task_name] = method_list.map {|m| method(m.to_sym)}
-  methods[task_name]
+def declare_methods(task_name, *method_list)
+  $methods[task_name] = method_list.map {|m| method(m.to_sym)}
+  $methods[task_name]
 end
 
 ############################################################
 ## The actual planner
 
-def rbhop(state, tasks, verbose=0, operators, methods)
+def rbhop(state, tasks, verbose=0)
   if verbose > 0
     puts "** rbhop, verbose = #{verbose}: **"
     puts "state = #{state.name}"
     puts "tasks = #{tasks}"
   end
-  result = seek_plan(state, tasks, [], 0, verbose, operators, methods)
+  result = seek_plan(state, tasks, [], 0, verbose)
   if verbose > 0 then puts "** result = #{result}" end
   result
 end
 
-def seek_plan(state, tasks, plan, depth, verbose=0, operators, methods)
+def seek_plan(state, tasks, plan, depth, verbose=0)
 
   if verbose > 1 then puts "depth #{depth} tasks #{tasks}" end
 
@@ -78,9 +81,9 @@ def seek_plan(state, tasks, plan, depth, verbose=0, operators, methods)
   end
 
   task1 = tasks[0]
-  if operators.has_key? task1[0]
+  if $operators.has_key? task1[0]
     if verbose > 2 then puts "depth #{depth} action #{task1}" end
-    operator = operators[task1[0]]
+    operator = $operators[task1[0]]
     newstate = operator.call(state.deep_clone, *task1.drop(1))
 
     if verbose > 2
@@ -89,14 +92,14 @@ def seek_plan(state, tasks, plan, depth, verbose=0, operators, methods)
     end
 
     if newstate
-      solution = seek_plan(newstate, tasks.drop(1), plan << task1, depth + 1, verbose, operators, methods)
+      solution = seek_plan(newstate, tasks.drop(1), plan << task1, depth + 1, verbose)
       unless solution == false then return solution end
     end
   end
 
-  if methods.has_key? task1[0]
+  if $methods.has_key? task1[0]
     if verbose > 2 then puts "depth #{depth} method instance #{task1}" end
-    relevant = methods[task1[0]]
+    relevant = $methods[task1[0]]
     relevant.each do |method|
       #spat operator
       subtasks = method.call(state, *task1.drop(1))
@@ -104,7 +107,9 @@ def seek_plan(state, tasks, plan, depth, verbose=0, operators, methods)
       if verbose > 2 then puts "depth #{depth} new tasks: #{subtasks}" end
 
       unless subtasks == false
-        solution = seek_plan(state, subtasks, plan, depth + 1, verbose, operators, methods)
+        #TODO: it should be something like "subtasks << tasks.drop(1)" 
+        #unstead of "subtasks" maybe
+        solution = seek_plan(state, subtasks, plan, depth + 1, verbose)
 
         unless solution == false then return solution end
       end
@@ -157,9 +162,7 @@ def pay_driver(state, a)
   end
 end
 
-declare_operators("walk", "call_taxi", "ride_taxi", "pay_driver", operators)
-puts ""
-#print_operators()
+declare_operators("walk", "call_taxi", "ride_taxi", "pay_driver")
 
 
 
@@ -177,9 +180,7 @@ def travel_by_taxi(state, a, x, y)
   false
 end
 
-declare_methods('travel', "travel_by_foot", "travel_by_taxi", methods)
-puts ""
-#rbhop.print_methods()
+declare_methods('travel', "travel_by_foot", "travel_by_taxi")
 
 class State
   attr_accessor :loc, :cash, :owe, :dist
@@ -199,14 +200,14 @@ Call rbhop.rbhop(state1,[('travel','me','home','park')]) with different verbosit
 "
 
 puts "", "- If verbose=0 (the default), Rbhop returns the solution but prints nothing."
-rbhop(state, [['travel', "me", "home", "park"]], operators, methods)
+rbhop(state, [['travel', "me", "home", "park"]])
 
 puts "", "- If verbose=1, Rbhop prints the problem and solution, and returns the solution:"
-rbhop(state, [["travel", "me", "home", "park"]], verbose=1, operators, methods)
+rbhop(state, [["travel", "me", "home", "park"]], verbose=1)
 
 puts "", "- If verbose=2, Rbhop also prints a note at each recursive call:"
-rbhop(state, [['travel', "me", "home", "park"]], verbose=2, operators, methods)
+rbhop(state, [['travel', "me", "home", "park"]], verbose=2)
 
 puts "", "- If verbose=3, Rbhop also prints the intermediate states:"
-rbhop(state, [['travel', "me", "home", "park"]], verbose=3, operators, methods)
+rbhop(state, [['travel', "me", "home", "park"]], verbose=3)
 
