64200c028936998a5d75c6eb667c54cbfd285b75
Quentin Bahers <quentin.bahers@gmail.com>
2014-04-02 11:50:51 +0200

    Refactor the code

--- rbhop.rb
+++ rbhop.rb
@@ -1,28 +1,36 @@
 ############################################################
-## States and goals
+# States and goals
 
 class State
   attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
+
   def deep_clone
-    Marshal::load(Marshal.dump(self))
+    Marshal.load(Marshal.dump(self))
   end
 end
 
 class Goal
   attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
+
   def deep_clone
-    Marshal::load(Marshal.dump(self))
+    Marshal.load(Marshal.dump(self))
   end
 end
 
-### print_state and print_goal are identical except for the name
+# print_state and print_goal are identical except for the name
 
 def print_state(state, indent=4)
   if state != false
     state.instance_variables.map do |var|
-      indent.times {print ' '}
+      indent.times { print " " }
       puts "#{var} = #{state.instance_variable_get(var)}"
     end
   else
@@ -33,7 +41,7 @@ end
 def print_goal(state, indent=4)
   if goal != false
     goal.instance_variables.map do |var|
-      indent.times {print ' '}
+      indent.times { print " " }
       puts "#{var} = #{goal.instance_variable_get(var)}"
     end
   else
@@ -42,7 +50,7 @@ def print_goal(state, indent=4)
 end
 
 ############################################################
-## Commands to tell Pyhop what the operators and methods are
+# Commands to tell Pyhop what the operators and methods are
 
 $operators = {}
 $methods = {}
@@ -53,12 +61,12 @@ def declare_operators(*op_list)
 end
 
 def declare_methods(task_name, *method_list)
-  $methods[task_name] = method_list.map {|m| method(m.to_sym)}
+  $methods[task_name] = method_list.map { |m| method(m.to_sym) }
   $methods[task_name]
 end
 
 ############################################################
-## The actual planner
+# The actual planner
 
 def rbhop(state, tasks, verbose=0)
   if verbose > 0
@@ -67,22 +75,23 @@ def rbhop(state, tasks, verbose=0)
     puts "tasks = #{tasks}"
   end
   result = seek_plan(state, tasks, [], 0, verbose)
-  if verbose > 0 then puts "** result = #{result}" end
+  puts "** result = #{result}" if verbose > 0
   result
 end
 
 def seek_plan(state, tasks, plan, depth, verbose=0)
 
-  if verbose > 1 then puts "depth #{depth} tasks #{tasks}" end
+  puts "depth #{depth} tasks #{tasks}" if verbose > 1
 
   if tasks == []
-    if verbose > 2 then puts "depth #{depth} returns plan #{plan}" end
+    puts "depth #{depth} returns plan #{plan}" if verbose > 2
     return plan
   end
 
   task1 = tasks[0]
-  if $operators.has_key? task1[0]
-    if verbose > 2 then puts "depth #{depth} action #{task1}" end
+
+  if $operators.key? task1[0]
+    puts "depth #{depth} action #{task1}" if verbose > 2
     operator = $operators[task1[0]]
     newstate = operator.call(state.deep_clone, *task1.drop(1))
 
@@ -93,31 +102,27 @@ def seek_plan(state, tasks, plan, depth, verbose=0)
 
     if newstate
       solution = seek_plan(newstate, tasks.drop(1), plan << task1, depth + 1, verbose)
-      unless solution == false then return solution end
+      return solution unless solution == false
     end
   end
 
-  if $methods.has_key? task1[0]
-    if verbose > 2 then puts "depth #{depth} method instance #{task1}" end
+  if $methods.key? task1[0]
+    puts "depth #{depth} method instance #{task1}" if verbose > 2
     relevant = $methods[task1[0]]
     relevant.each do |method|
-      #spat operator
       subtasks = method.call(state, *task1.drop(1))
-      # ...
-      if verbose > 2 then puts "depth #{depth} new tasks: #{subtasks}" end
+      puts "depth #{depth} new tasks: #{subtasks}" if verbose > 2
 
       unless subtasks == false
-        #TODO: it should be something like "subtasks << tasks.drop(1)" 
-        #instead of "subtasks" maybe
+        # TODO: it should be something like "subtasks << tasks.drop(1)" 
+        # instead of "subtasks" maybe
         solution = seek_plan(state, subtasks, plan, depth + 1, verbose)
-
-        unless solution == false then return solution end
+        return solution unless solution == false
       end
     end
   end
 
-  if verbose > 2 then puts "depth #{depth} returns failure" end
+  puts "depth #{depth} returns failure" if verbose > 2
   false
 end
-
 
--- simple_travel_example.rb
+++ simple_travel_example.rb
@@ -1,7 +1,6 @@
-require_relative 'rbhop'
+# The "travel from home to the park" example
 
-############################################################
-## The "travel from home to the park" example
+require_relative "rbhop"
 
 def taxi_rate(dist)
   1.5 + 0.5 * dist
@@ -10,7 +9,7 @@ end
 def walk(state, a, x, y)
   if state.loc[a] == x
     state.loc[a] = y
-    return state
+    state
   else
     false
   end
@@ -22,11 +21,11 @@ def call_taxi(state, a, x)
 end
 
 def ride_taxi(state, a, x, y)
-  if state.loc["taxi"] == x and state.loc[a] == x
+  if state.loc["taxi"] == x && state.loc[a] == x
     state.loc["taxi"] = y
     state.loc[a] = y
     state.owe[a] = taxi_rate(state.dist[x][y])
-    return state
+    state
   else
     false
   end
@@ -36,7 +35,7 @@ def pay_driver(state, a)
   if state.cash[a] >= state.owe[a]
     state.cash[a] = state.cash[a] - state.owe[a]
     state.owe[a] = 0
-    return state
+    state
   else
     false
   end
@@ -48,30 +47,29 @@ declare_operators("walk", "call_taxi", "ride_taxi", "pay_driver")
 
 def travel_by_foot(state, a, x, y)
   if state.dist[x][y] <= 2
-    return [['walk', a, x, y]]
+    return [["walk", a, x, y]]
   end
   false
 end
 
 def travel_by_taxi(state, a, x, y)
   if state.cash[a] >= taxi_rate(state.dist[x][y])
-    return [['call_taxi',a,x], ['ride_taxi',a,x,y], ['pay_driver',a]]
+    return [["call_taxi",a,x], ["ride_taxi",a,x,y], ["pay_driver",a]]
   end
   false
 end
 
-declare_methods('travel', "travel_by_foot", "travel_by_taxi")
+declare_methods("travel", "travel_by_foot", "travel_by_taxi")
 
-class State
+class StateTravel < State
   attr_accessor :loc, :cash, :owe, :dist
 end
 
-state = State.new
-state.name = "state1"
-state.loc = {"me" => "home"}
-state.cash = {"me" => 20}
-state.owe = {"me" => 0}
-state.dist = {"home" => {"park" => 8}, "park" => {"home" => 8}}
+state = StateTravel.new("state1")
+state.loc  = { "me" => "home" }
+state.cash = { "me" => 20 }
+state.owe  = { "me" => 0 }
+state.dist = { "home" => { "park" => 8 }, "park" => { "home" => 8 } }
 
 puts "
 ********************************************************************************
@@ -80,14 +78,14 @@ Call rbhop.rbhop(state1,[('travel','me','home','park')]) with different verbosit
 "
 
 puts "", "- If verbose=0 (the default), Rbhop returns the solution but prints nothing."
-rbhop(state, [['travel', "me", "home", "park"]])
+rbhop(state, [["travel", "me", "home", "park"]])
 
 puts "", "- If verbose=1, Rbhop prints the problem and solution, and returns the solution:"
 rbhop(state, [["travel", "me", "home", "park"]], verbose=1)
 
 puts "", "- If verbose=2, Rbhop also prints a note at each recursive call:"
-rbhop(state, [['travel', "me", "home", "park"]], verbose=2)
+rbhop(state, [["travel", "me", "home", "park"]], verbose=2)
 
 puts "", "- If verbose=3, Rbhop also prints the intermediate states:"
-rbhop(state, [['travel', "me", "home", "park"]], verbose=3)
+rbhop(state, [["travel", "me", "home", "park"]], verbose=3)
 
