fce991697fabda65a3f6762777324f8dd4120f4f
Quentin Bahers <quentin.bahers@gmail.com>
2014-04-24 14:37:49 +0200

    Put the API module inside the Rbhop module

--- lib/rbhop.rb
+++ lib/rbhop.rb
@@ -6,45 +6,47 @@ require "rbhop/api"
 require "rbhop/version"
 require "rbhop/simple_travel/simple_travel"
 
-def sanitize(word)
-  word.gsub(/[^0-9A-Za-z]/, "")
-      .downcase
-end
-
-def find_domain(plain_text)
-  store = YAML::Store.new("../lib/rbhop/data.yml")
-
-  planning_domains = store.transaction(true) do
-    store["planning_domains"]
+module Rbhop
+  def self.sanitize(word)
+    word.gsub(/[^0-9A-Za-z]/, "")
+        .downcase
   end
 
-  words = plain_text.split(" ")
+  def self.find_domain(plain_text)
+    store = YAML::Store.new("../lib/rbhop/data.yml")
 
-  words.each do |word|
-    s_word = sanitize(word)
-    return planning_domains[s_word] if planning_domains.key?(s_word)
-  end
-  false
-end
-
-def rbhop(plain_text)
-  domain = find_domain(plain_text)
-
-  if domain
-    requirements = domain["requirements"]
-
-    requirements.map! do |requirement|
-      value = API.get_last_value(requirement)
-      puts "#{requirement}: #{value}"
-      value
+    planning_domains = store.transaction(true) do
+      store["planning_domains"]
     end
 
-    state_name = domain["state_name"]
-    state = Kernel.const_get(state_name).new(*requirements)
-    params = domain["params"]
-    plan = ai_plan(state, params)
-  else
-    "(._.?)"
+    words = plain_text.split(" ")
+
+    words.each do |word|
+      s_word = sanitize(word)
+      return planning_domains[s_word] if planning_domains.key?(s_word)
+    end
+    false
+  end
+
+  def self.rbhop(plain_text)
+    domain = find_domain(plain_text)
+
+    if domain
+      requirements = domain["requirements"]
+
+      requirements.map! do |requirement|
+        value = Rbhop::API.get_last_value(requirement)
+        puts "#{requirement}: #{value}"
+        value
+      end
+
+      state_name = domain["state_name"]
+      state = Kernel.const_get(state_name).new(*requirements)
+      params = domain["params"]
+      plan = ai_plan(state, params)
+    else
+      "(._.?)"
+    end
   end
 end
 
--- lib/rbhop/api.rb
+++ lib/rbhop/api.rb
@@ -1,58 +1,60 @@
 require "net/http"
 require "json"
 
-module API
+module Rbhop
+  module API
 
-  IOT_FRAMEWORK_URI = "http://localhost:8000"
+    IOT_FRAMEWORK_URI = "http://localhost:8000"
 
-  def self.get_streams(tag)
-    uri = URI("#{IOT_FRAMEWORK_URI}/streams/_search")
-    parsed_body = { query: { term: { tags: tag } } }
+    def self.get_streams(tag)
+      uri = URI("#{IOT_FRAMEWORK_URI}/streams/_search")
+      parsed_body = { query: { term: { tags: tag } } }
 
-    req = Net::HTTP::Post.new(uri.path)
-    req.body = parsed_body.to_json
-    req.content_type = "application/json"
+      req = Net::HTTP::Post.new(uri.path)
+      req.body = parsed_body.to_json
+      req.content_type = "application/json"
 
-    res = Net::HTTP.start(uri.host, uri.port) do |http|
-      http.request(req)
+      res = Net::HTTP.start(uri.host, uri.port) do |http|
+        http.request(req)
+      end
+
+      case res
+      when Net::HTTPSuccess, Net::HTTPRedirection
+        # OK
+        body = res.body
+        parsed_body = JSON.parse(body)
+        streams = parsed_body["hits"]["hits"]
+      else
+        res.value
+      end
     end
 
-    case res
-    when Net::HTTPSuccess, Net::HTTPRedirection
-      # OK
-      body = res.body
-      parsed_body = JSON.parse(body)
-      streams = parsed_body["hits"]["hits"]
-    else
-      res.value
+    def self.get_last_datapoint(stream_id)
+      uri = URI("#{IOT_FRAMEWORK_URI}/streams/#{stream_id}/data")
+
+      req = Net::HTTP::Get.new(uri.path)
+
+      res = Net::HTTP.start(uri.host, uri.port) do |http|
+        http.request(req)
+      end
+
+      case res
+      when Net::HTTPSuccess, Net::HTTPRedirection
+        # OK
+        body = res.body
+        parsed_body = JSON.parse(body)
+        data_points = parsed_body["data"]
+        sorted_data_points = data_points.sort_by { |hsh| hsh["timestamp"] }
+        last_data_point = sorted_data_points.last["value"]
+      else
+        res.value
+      end
     end
-  end
 
-  def self.get_last_datapoint(stream_id)
-    uri = URI("#{IOT_FRAMEWORK_URI}/streams/#{stream_id}/data")
-
-    req = Net::HTTP::Get.new(uri.path)
-
-    res = Net::HTTP.start(uri.host, uri.port) do |http|
-      http.request(req)
+    def self.get_last_value(tag)
+      streams = get_streams(tag)
+      first_stream_id = streams[0]["_id"]
+      data_point = get_last_datapoint(first_stream_id)
     end
-
-    case res
-    when Net::HTTPSuccess, Net::HTTPRedirection
-      # OK
-      body = res.body
-      parsed_body = JSON.parse(body)
-      data_points = parsed_body["data"]
-      sorted_data_points = data_points.sort_by { |hsh| hsh["timestamp"] }
-      last_data_point = sorted_data_points.last["value"]
-    else
-      res.value
-    end
-  end
-
-  def self.get_last_value(tag)
-    streams = get_streams(tag)
-    first_stream_id = streams[0]["_id"]
-    data_point = get_last_datapoint(first_stream_id)
   end
 end
